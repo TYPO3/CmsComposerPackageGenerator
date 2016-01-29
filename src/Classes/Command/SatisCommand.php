e2be55eaf822c259ce4f3a416b3b0cc8256c1c9b
Alexander Schnitzler <git@alexanderschnitzler.de>
2016-01-29 17:59:31 +0100

    [TASK] Remove TER repository url support
    Due to possible security issues that are too hard
    to fix, that feature should rather be removed again.

--- src/Classes/Command/SatisCommand.php
+++ src/Classes/Command/SatisCommand.php
@@ -19,17 +19,8 @@ use TYPO3\Composer\Model\SatisJson;
 use Guzzle\Http\Client;
 use Webmozart\Json\JsonDecoder;
 
-/**
- * Class SatisCommand
- * @package TYPO3\Composer\Command
- */
 class SatisCommand extends \Symfony\Component\Console\Command\Command
 {
-
-    /**
-     * @var string
-     */
-    const REPOSITORIES_URL = 'http://typo3.org/?eID=ter_fe2:extension&action=findAllWithRepositoryUrlAsPackageSource';
 
     /**
      * @return void
@@ -60,19 +51,16 @@ class SatisCommand extends \Symfony\Component\Console\Command\Command
             throw new \RuntimeException(sprintf('File "%s" is not writable', $filePath), 1438441994);
         }
 
-        $repositories = array_merge(
-            array(
-                array(
-                    'type' => 'composer',
-                    'url' => 'file://' . $webroot . '/Web/packages-TYPO3Extensions-archive.json'
-                ),
-                array(
-                    'type' => 'composer',
-                    'url' => 'file://' . $webroot . '/Web/packages-TYPO3Extensions-quarter.json'
-                )
-            ),
-            $this->fetchRepositories()
-        );
+        $repositories = array(
+              array(
+                  'type' => 'composer',
+                  'url' => 'file://' . $webroot . '/Web/packages-TYPO3Extensions-archive.json'
+              ),
+              array(
+                  'type' => 'composer',
+                  'url' => 'file://' . $webroot . '/Web/packages-TYPO3Extensions-quarter.json'
+              )
+          );
 
         $satis = new SatisJson('TYPO3 Extension Repository');
         $satis->setHomepage('https://composer.typo3.org');
@@ -85,39 +73,5 @@ class SatisCommand extends \Symfony\Component\Console\Command\Command
         } else {
             $output->writeln(sprintf('Successfully created "%s"', $filePath));
         }
-    }
-
-    /**
-     * @return array
-     * @throws \Exception
-     * @throws \Webmozart\Json\ValidationFailedException
-     */
-    protected function fetchRepositories()
-    {
-        $client = new Client();
-        $request = $client->get(static::REPOSITORIES_URL);
-        $response = $request->send();
-        $responseBody = $response->getBody(true);
-
-        $jsonDecoder = new JsonDecoder();
-        $jsonDecoder->setObjectDecoding(JsonDecoder::ASSOC_ARRAY);
-
-        $json = $jsonDecoder->decode($responseBody);
-
-        if (null !== $json['meta']) {
-            throw new \Exception($json['meta']['error']);
-        }
-
-        $repositories = array();
-        if (is_array($json['data'])) {
-            foreach ($json['data'] as $extKey => $extData) {
-                $repositories[] = array(
-                    'type' => 'vcs',
-                    'url' => $extData['repository_clone_url']
-                );
-            }
-        }
-
-        return $repositories;
     }
 }
