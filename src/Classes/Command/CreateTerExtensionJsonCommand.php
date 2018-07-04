3fc838ea549a46b875dddf5817225f495db6ac72
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2018-07-04 07:31:34 +0200

    [TASK] Adds aoe_ipauth to typo3-ter abandon extensions

--- src/Classes/Command/CreateTerExtensionJsonCommand.php
+++ src/Classes/Command/CreateTerExtensionJsonCommand.php
@@ -66,6 +66,7 @@ class CreateTerExtensionJsonCommand extends \Symfony\Component\Console\Command\C
      * @var array
      */
     protected static $abandonedExtensionKeys = [
+      'aoe_ipauth' => 'aoe/aoe_ipauth',
       'news' => 'georgringer/news',
       'typo3_console' => 'helhum/typo3-console',
     ];
