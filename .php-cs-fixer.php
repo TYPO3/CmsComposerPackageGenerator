e144b97103b32cf6d43ecebf5ec0d2429fcbe884
Simon Gilli <25326036+gilbertsoft@users.noreply.github.com>
2021-07-18 15:29:47 +0200

    [TASK] Update and apply CS-Fixer rules

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,30 @@
 <?php
 
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->getFinder()
-    ->in(__DIR__ . '/src/')
-    ->in(__DIR__ . '/Tests/');
+$config
+    ->setHeader(
+        <<<EOM
+This file is part of the package typo3/cms-composer-package-generator.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+For the full copyright and license information, please read the
+LICENSE file that was distributed with this source code.
+
+The TYPO3 project - inspiring people to share!
+EOM,
+        true)
+    ->getFinder()
+        ->exclude(['Build', 'Data', 'tools', 'var', 'vendor', 'Web'])
+        ->in(__DIR__)
+;
+
 return $config;
--- src/Classes/Command/SatisCommand.php
+++ src/Classes/Command/SatisCommand.php
@@ -1,16 +1,24 @@
 <?php
+
 /*
- * This file is part of the TYPO3 CMS project.
+ * This file is part of the package typo3/cms-composer-package-generator.
  *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
+ * LICENSE file that was distributed with this source code.
  *
  * The TYPO3 project - inspiring people to share!
  */
+
 namespace TYPO3\Composer\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
